b90d65d2af113b2c74d65ef8b8a7c7704d9f3a14
Varenya Jain <78588847+VarenyaJ@users.noreply.github.com>
2022-05-23 11:07:45 -0500

    add bam option

--- testing_command_script/random-trial-click-test.py
+++ testing_command_script/random-trial-click-test.py
@@ -1,9 +1,19 @@
+#1>>redo options with click.option
 import click
 @click.command()
-@click.argument('filename', type=click.Path(exists=True))
-def touch(filename):
-	"""Print FILENAME if the file exists."""
-	click.echo(click.format_filename(filename))
+#no capital letters:
+@click.option('--file_path', default = "/home/suvinit/NEAT-data/H1N1/H1N1.fa", help = 'Path to reference fasta', type=click.Path(exists=True))
 
-@click.option('--R', default = 100, help = 'The desired read length')
-@click.option('--o', default = /home/suvinit/Desktop/command-script-test-run-H1N1-1, help = 'output_prefix')
+@click.option('--read_length', default = 100, help = 'The desired read length')
+#try to test is ^ this is within a certain range (50,300)
+
+@click.option('--output_path', default = "/home/suvinit/NEAT-data/H1N1/H1N1.test-run", help = 'output_prefix')
+@click.option('--bam', default = "/home/suvinit/NEAT/testing_command_script/test-output-H1N1.bam", help='output golden BAM file')
+################################<TASK BELOW>################################
+#try validating that the file directory exists (/home/suvinit)
+
+def hello(file_path, read_length, output_path, bam):
+	click.echo(f'Hello {file_path}' + f'\nHello {read_length}' + f'\nHello {output_path}')
+	click.echo(f'\noutput bam test line:\n {bam}')
+if __name__ == '__main__':
+	hello()
--- testing_command_script/test_command.py
+++ testing_command_script/test_command.py
@@ -8,18 +8,14 @@ import click
 #try to test is ^ this is within a certain range (50,300)
 
 @click.option('--output_path', default = "/home/suvinit/NEAT-data/H1N1/H1N1.test-run", help = 'output_prefix')
-################################<TASK BELOW>################################
-#try validating that the file directory exists (/home/suvinit)
 
-def hello(file_path, read_length, output_path):
+@click.option('--bam', default = "/home/suvinit/NEAT/testing_command_script/test-output-H1N1.bam", help='output golden BAM file')
+
+def hello(file_path, read_length, output_path, bam):
 	click.echo(f'Hello {file_path}' + f'\nHello {read_length}' + f'\nHello {output_path}')
+	click.echo(f'\noutput bam test line:\n {bam}')
 if __name__ == '__main__':
 	hello()
 ################################<TASK BELOW>################################
-#try adding bam
+#try making the "--bam" optional
 #parser.add_argument('--bam', required=False, action='store_true', default=False, help='output golden BAM file')
-@click.option('--bam', default = "/home/suvinit/NEAT/testing_command_script/test-output-H1N1.bam", help='output golden BAM file')
-def hi(bam):
-	click.echo(f'\noutput bam test line:\n {bam}')
-if __name__ == "__main__":
-	hi()
